5d63234b82791f666a6f9689ea6c7808eff81fb3
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-01 21:47:47 -0300

    Modelagem de Sorteio

--- src/OnlinePremios.Domain/Entities/Sorteio.cs
+++ src/OnlinePremios.Domain/Entities/Sorteio.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using OnlinePremios.Domain.Core;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace OnlinePremios.Domain.Entities
 {
-    class Sorteio
+    public class Sorteio : EntityBase
     {
+        protected Sorteio(int numero, DateTime dataInicial, DateTime dataSorteio, int qtdeCotas)
+        {
+            this.Numero = numero;
+            this.DataInicial = dataInicial;
+            this.DataSorteio = dataSorteio;
+            this.QtdeCotas = qtdeCotas;
+        }
+
+        public int Numero { get; private set; }
+        public DateTime DataInicial { get; private set; }
+        public DateTime DataSorteio { get; private set; }
+        public int QtdeCotas { get; private set; }
+
     }
 }
