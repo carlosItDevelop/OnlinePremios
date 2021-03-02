6beed42ad45b562305caeffab5ff827a8d2cb783
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-01 21:14:33 -0300

    protected Compra Created

---
+++ src/OnlinePremios.Domain/Entities/Compra.cs
@@ -0,0 +1,25 @@
+﻿using OnlinePremios.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlinePremios.Domain.Entities
+{
+    public class Compra : EntityBase
+    {
+
+        protected Compra(Guid compradorId, DateTime dataCompra, int qtdeCotas)
+        {
+
+            // Validar id != null, dataValida e qtde > 0;
+
+            this.CompradorId = compradorId;
+            this.DataCompra = dataCompra;
+            this.QtdeCotas = qtdeCotas;
+        }
+
+        public Guid CompradorId { get; private set; }
+        public DateTime DataCompra { get; private set; }
+        public int QtdeCotas { get; set; }
+    }
+}
