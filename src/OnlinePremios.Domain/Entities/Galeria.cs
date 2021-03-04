eaa7c62d8aaf862da3c5323ddc02893a41e00230
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-04 01:31:13 -0300

    Galeria com set privado e SorteioId recebendo via Ctor

--- src/OnlinePremios.Domain/Entities/Galeria.cs
+++ src/OnlinePremios.Domain/Entities/Galeria.cs
@@ -6,12 +6,12 @@ namespace OnlinePremios.Domain.Entities
 {
     public class Galeria : EntityBase
     {
-        public Galeria()
+        public Galeria(Guid sorteioId)
         {
-
+            this.SorteioId = sorteioId;
         }
 
-        public Guid SorteioId { get; set; }
+        public Guid SorteioId { get; private set; }
         public virtual Sorteio Sorteio { get; set; }
         public virtual ICollection<Produto> Produtos { get; set; }
     }
