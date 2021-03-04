a7981358089c1ea79ee0dc0894afa25a9e969c2d
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-04 18:37:49 -0300

    CotaViewModel Created and configurated

--- src/OnlinePremio.Mvc/ViewModels/CotaViewModel.cs
+++ src/OnlinePremio.Mvc/ViewModels/CotaViewModel.cs
@@ -1,6 +1,32 @@
-﻿namespace OnlinePremio.Mvc.ViewModels
+﻿using OnlinePremios.Domain.Entities;
+using OnlinePremios.Domain.Enum;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlinePremio.Mvc.ViewModels
 {
     public class CotaViewModel
     {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [Display(Name = "Número")]
+        public int Numero { get; private set; }
+
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [Display(Name = "Valor")]
+        public decimal Valor { get; private set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [Display(Name = "Status")]
+        public StatusCota StatusCota { get; private set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [Display(Name = "Compra ID")]
+        public Guid CompraId { get; private set; }
+
+        public virtual Compra Compra { get; set; }
     }
 }
