11b0566c2cc71c6614b410f94c96c61d4009f901
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-04 18:24:16 -0300

    CotaMap created and configurated

---
+++ src/OnlinePremios.Data/Mapping/CotaMap.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlinePremios.Domain.Entities;
+
+namespace OnlinePremios.Data.Mapping
+{
+    public class CotaMap : IEntityTypeConfiguration<Cota>
+    {
+        public void Configure(EntityTypeBuilder<Cota> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Numero).IsRequired().HasColumnName("Numero");
+            builder.Property(x => x.StatusCota).IsRequired().HasColumnName("StatusCota");
+            builder.Property(x => x.Valor).IsRequired().HasColumnName("Valor");
+
+            builder.ToTable("Cota");
+
+            // 1:N Compras : Cota
+            builder.HasOne(x => x.Compra)
+                .WithMany(x => x.Cotas)
+                .HasForeignKey(x => x.CompraId);
+        }
+    }
+}
