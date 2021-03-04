de4ee029803c8e078a06661599bce78521e33748
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-03 22:30:41 -0300

    Update  StatusSorteio in Sorteio of the int to enum

--- src/OnlinePremios.Domain/Entities/Sorteio.cs
+++ src/OnlinePremios.Domain/Entities/Sorteio.cs
@@ -1,4 +1,5 @@
 ﻿using OnlinePremios.Domain.Core;
+using OnlinePremios.Domain.Enum;
 using System;
 
 namespace OnlinePremios.Domain.Entities
@@ -6,20 +7,20 @@ namespace OnlinePremios.Domain.Entities
     public class Sorteio : EntityBase
     {
         protected Sorteio(int numero, DateTime dataInicial, 
-            DateTime dataSorteio, int qtdeCotas, int status)
+            DateTime dataSorteio, int qtdeCotas)
         {
             this.Numero = numero;
             this.DataInicial = dataInicial;
             this.DataSorteio = dataSorteio;
             this.QtdeCotas = qtdeCotas;
-            this.StatusSorteio = status;
+            // Setar StatusSorteio
         }
 
         public int Numero { get; private set; }
         public DateTime DataInicial { get; private set; }
         public DateTime DataSorteio { get; private set; }
         public int QtdeCotas { get; private set; }
-        public int StatusSorteio { get; set; }
+        public StatusSorteio StatusSorteio { get; set; }
 
         public Guid GaleriaId { get; set; }
         public virtual Galeria Galeria { get; set; }
