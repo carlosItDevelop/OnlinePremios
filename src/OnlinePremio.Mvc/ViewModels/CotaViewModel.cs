471247e5f727e0309b857e2dfc7dfd9ffa5995fb
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-04 18:58:27 -0300

    Add DataCadastr, DataAtualizacao and UserUpdate in CotaViewModel

--- src/OnlinePremio.Mvc/ViewModels/CotaViewModel.cs
+++ src/OnlinePremio.Mvc/ViewModels/CotaViewModel.cs
@@ -10,6 +10,17 @@ namespace OnlinePremio.Mvc.ViewModels
         [Key]
         public Guid Id { get; set; }
 
+        [Display(Name = "Data do Cadastro")]
+        public DateTime? DataCadastro { get; set; }
+        [Display(Name = "Data de Atualização")]
+        public DateTime? DataAtualizacao { get; set; }
+        [Display(Name = "Usuário da Atualização")]
+        public Guid? UserUpdate { get; set; }
+
+        // =================================
+
+
+
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         [Display(Name = "Número")]
         public int Numero { get; private set; }
