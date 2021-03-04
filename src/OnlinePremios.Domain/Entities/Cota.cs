d910c634829f71a853abb220c023d9edbb7bbc36
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-04 01:16:17 -0300

    StatusCota setada, Add Valor

--- src/OnlinePremios.Domain/Entities/Cota.cs
+++ src/OnlinePremios.Domain/Entities/Cota.cs
@@ -6,16 +6,18 @@ namespace OnlinePremios.Domain.Entities
 {
     public class Cota : EntityBase
     {
-        public Cota(int numero, Guid compraId)
+        public Cota(int numero, Guid compraId, decimal valor)
         {
             this.Numero = numero;
             this.CompraId = compraId;
-            // Setar StatusCota
+            this.Valor = valor;
+            this.StatusCota = StatusCota.Disponivel;
         }
 
         public int Numero { get; private set; }
+        public decimal Valor { get; private set; }
         public StatusCota StatusCota { get; private set; }
-        public Guid CompraId { get; set; }
+        public Guid CompraId { get; private set; }
         public virtual Compra Compra { get; private set; }
 
     }
