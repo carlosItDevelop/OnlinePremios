2b2dd617c9a6b1ebaba045b00e7abfe62f55612d
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-04 19:05:22 -0300

    Created ProdutoViewModel Attributes

--- src/OnlinePremio.Mvc/ViewModels/ProdutoViewModel.cs
+++ src/OnlinePremio.Mvc/ViewModels/ProdutoViewModel.cs
@@ -1,6 +1,39 @@
-﻿namespace OnlinePremio.Mvc.ViewModels
+﻿using OnlinePremios.Domain.Entities;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlinePremio.Mvc.ViewModels
 {
     public class ProdutoViewModel
     {
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Data do Cadastro")]
+        public DateTime? DataCadastro { get; set; }
+        [Display(Name = "Data de Atualização")]
+        public DateTime? DataAtualizacao { get; set; }
+        [Display(Name = "Usuário da Atualização")]
+        public Guid? UserUpdate { get; set; }
+
+        // =================================
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório.")]
+        [Display(Name = "Img URL")]
+        [StringLength(2000, ErrorMessage = "O campo {0} deve ter entre {2} e (1} caracteres", MinimumLength = 5)]
+        public string ImgURL { get; set; }
+        // Todo: file img (vou pensar se gravo no banco ou no disco;
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório.")]
+        [Display(Name = "Nome do Arquivo")]
+        [StringLength(400, ErrorMessage = "O campo {0} deve ter entre {2} e (1} caracteres", MinimumLength = 3)]
+        public string FileNome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Display(Name = "Galeria ID")]
+        public Guid GaleriaId { get; set; }
+        public virtual Galeria Galeria { get; set; }
+
     }
 }
