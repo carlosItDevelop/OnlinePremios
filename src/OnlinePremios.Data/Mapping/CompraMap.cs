266ddc250683ad279c88c4136f47917142d1d7fd
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-04 01:37:50 -0300

    CompraMap criado, falta configurar

---
+++ src/OnlinePremios.Data/Mapping/CompraMap.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlinePremios.Domain.Entities;
+using System;
+
+namespace OnlinePremios.Data.Mapping
+{
+    public class CompraMap : IEntityTypeConfiguration<Compra>
+    {
+        public void Configure(EntityTypeBuilder<Compra> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
