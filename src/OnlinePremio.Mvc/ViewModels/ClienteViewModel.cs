685cb20b9a7d262b16a9ccafb0a536b0ddb9b1f7
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-04 05:28:27 -0300

    Automapper 8.1.0 installed

--- src/OnlinePremio.Mvc/ViewModels/ClienteViewModel.cs
+++ src/OnlinePremio.Mvc/ViewModels/ClienteViewModel.cs
@@ -1,6 +1,13 @@
 ﻿namespace OnlinePremio.Mvc.ViewModels
 {
+    /// <summary>
+    /// Todo: Preciso definir as raizes de agregações
+    ///    Exemplo:
+    ///    Cliente tem Compra, tem Pagamento(nem criei)
+    ///    Compra tem Cota, Galeria, Produto...
+    /// </summary>
     public class ClienteViewModel
-    {
+    {        
+        
     }
 }
