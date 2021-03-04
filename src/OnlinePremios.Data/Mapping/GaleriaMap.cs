a465883a3adab9f7485e73f98f8ae364be1f7732
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-04 01:28:24 -0300

    GaleriaMap criado com Relacionamento 1:1 com Sorteio e 1:N com Produto

---
+++ src/OnlinePremios.Data/Mapping/GaleriaMap.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlinePremios.Domain.Entities;
+
+namespace OnlinePremios.Data.Mapping
+{
+    public class GaleriaMap : IEntityTypeConfiguration<Galeria>
+    {
+        public void Configure(EntityTypeBuilder<Galeria> builder)
+        {
+            builder.HasKey(pk => pk.Id);
+            builder.Property(p => p.SorteioId).HasColumnName("SorteioId").IsRequired();
+
+            // 1:1 Sorteio
+            builder.HasOne(s => s.Sorteio)
+                .WithOne(fk => fk.Galeria);
+
+            // 1 : N => Produtos
+            builder.HasMany(g => g.Produtos)
+                .WithOne(g => g.Galeria)
+                .HasForeignKey(fk => fk.GaleriaId);
+
+            builder.ToTable("Galeria");
+
+        }
+    }
+}
