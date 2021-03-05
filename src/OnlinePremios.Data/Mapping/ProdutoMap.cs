044dbaf2b7e3b5b5f96db84aa1f63b797c50b364
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-04 21:51:02 -0300

    past properties commented for configure...

--- src/OnlinePremios.Data/Mapping/ProdutoMap.cs
+++ src/OnlinePremios.Data/Mapping/ProdutoMap.cs
@@ -10,6 +10,13 @@ namespace OnlinePremios.Data.Mapping
         {
             builder.HasKey(x => x.Id);
 
+            /*
+                public string ImgURL { get; set; }
+                // Todo: file img (vou pensar se gravo no banco ou no disco;
+                public string FileNome { get; set; }
+                public Guid GaleriaId { get; set; }
+                public virtual Galeria Galeria { get; set; }             
+             */
 
             builder.ToTable("Produto");
         }
