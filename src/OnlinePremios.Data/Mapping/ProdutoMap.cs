8d4b63bc7b0ffc3e5aa57b7d15c58930a86ecca6
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-04 21:49:40 -0300

    Created ProdutoMap with HasKey and ToTable - Todo Refactory for other Properties and relationship

---
+++ src/OnlinePremios.Data/Mapping/ProdutoMap.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlinePremios.Domain.Entities;
+
+namespace OnlinePremios.Data.Mapping
+{
+    public class ProdutoMap : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+
+            builder.ToTable("Produto");
+        }
+    }
+}
