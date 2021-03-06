502b1085ef6de47a0580af13b13484c5081253f4
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-05 22:04:38 -0300

    Created IProdutoService

---
+++ src/OnlinePremios.Domain/Interfaces/IProdutoService.cs
@@ -0,0 +1,13 @@
+﻿using OnlinePremios.Domain.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace OnlinePremios.Domain.Interfaces
+{
+    public interface IProdutoService : IDisposable
+    {
+        Task Add(Produto produto);
+        Task Update(Produto produto);
+        Task Remove(Guid id);
+    }
+}
