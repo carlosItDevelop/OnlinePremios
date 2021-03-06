f3062f37e0547daecb2bd38e74e8fa3b15818932
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-06 02:08:50 -0300

    Ctrl R+Ctrl G GaleriaValidation, IGereriaRepository, IGaleriaService, GaleriaService, GaleriaRepository

--- src/OnlinePremios.Domain/Entities/Validations/GaleriaValidation.cs
+++ src/OnlinePremios.Domain/Entities/Validations/GaleriaValidation.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace OnlinePremios.Domain.Entities.Validations
+﻿namespace OnlinePremios.Domain.Entities.Validations
 {
     class GaleriaValidation
     {
--- src/OnlinePremios.Domain/Interfaces/IGaleriaRepository.cs
+++ src/OnlinePremios.Domain/Interfaces/IGaleriaRepository.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace OnlinePremios.Domain.Interfaces
+﻿namespace OnlinePremios.Domain.Interfaces
 {
     class IGaleriaRepository
     {
