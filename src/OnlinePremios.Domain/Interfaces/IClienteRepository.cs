bd60c0f350e1492226480a52f7545fb3086789a7
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-07 00:41:53 -0300

    first version IClienteRepository ready

---
+++ src/OnlinePremios.Domain/Interfaces/IClienteRepository.cs
@@ -0,0 +1,14 @@
+﻿using OnlinePremios.Domain.Core;
+using OnlinePremios.Domain.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace OnlinePremios.Domain.Interfaces
+{
+    public interface IClienteRepository : IGenericRepository<Cliente, Guid>
+    {
+        Task<Galeria> ObterUmClienteComSuasCotas(Guid id);
+        Task<Galeria> ObterUmClienteComSorteioECotas(Guid id);
+    }
+
+}
