5776fd0f077c5fed1624654845588298d79963da
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-07 18:23:19 -0300

    Created ICotaRepository

---
+++ src/OnlinePremios.Domain/Interfaces/ICotaRepository.cs
@@ -0,0 +1,17 @@
+﻿using OnlinePremios.Domain.Core;
+using OnlinePremios.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OnlinePremios.Domain.Interfaces
+{
+    public interface ICotaRepository : IGenericRepository<Cota, Guid>
+    {
+
+        Task<IEnumerable<Cota>> ObterTodasAsCotasDeUmaCompra(Guid compraId);
+        Task<IEnumerable<Cota>> ObterTodasAsCotasComSuasCompras();
+        Task<Cota> ObterUmaCotaComSuaCompra(Guid id);
+    }
+
+}
