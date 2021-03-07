b3e645d50103e023f95dcd3a4d8a38a7054a2e0c
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-07 18:08:42 -0300

    Created CotaValidation

---
+++ src/OnlinePremios.Domain/Entities/Validations/CotaValidation.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using System;
+
+
+namespace OnlinePremios.Domain.Entities.Validations
+{
+
+    public class CotaValidation : AbstractValidator<Cota>
+    {
+        public CotaValidation()
+        {
+            RuleFor(f => f.Numero)
+                .NotNull().WithMessage("O campo {PropertyName} precisa ser fornecido");
+
+            RuleFor(f => f.Valor)
+                .NotNull().WithMessage("O campo {PropertyName} precisa ser fornecido");
+
+            RuleFor(f => f.StatusCota)
+                .NotNull().WithMessage("O campo {PropertyName} precisa ser fornecido");
+
+            RuleFor(f => f.CompraId)
+                .NotNull().WithMessage("O campo {PropertyName} precisa ser fornecido");
+
+
+        }
+    }
+
+}
