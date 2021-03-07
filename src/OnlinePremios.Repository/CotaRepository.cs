597614f74860ab018aa5ff93ac3d1c91204ab76a
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-07 18:44:20 -0300

    Created concrete implementation CotaRepository

---
+++ src/OnlinePremios.Repository/CotaRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlinePremios.Data.Orm;
+using OnlinePremios.Domain.Entities;
+using OnlinePremios.Domain.Interfaces;
+using OnlinePremios.Repository.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlinePremios.Repository
+{
+    public class CotaRepository : RepositoryGeneric<Cota, Guid>, ICotaRepository
+    {
+        private readonly OnlinePremiosContext _ctx;
+        public CotaRepository(OnlinePremiosContext context) : base(context) 
+        {
+            this._ctx = context;
+        }
+
+        public virtual async Task<IEnumerable<Cota>> ObterTodasAsCotasComSuasCompras()
+        {
+            return await _ctx.Cota.AsNoTracking().Include(f => f.Compra)
+                .OrderBy(p => p.DataCadastro).ToListAsync();
+        }
+
+        public virtual async Task<IEnumerable<Cota>> ObterTodasAsCotasDeUmaCompra(Guid compraId)
+        {
+            return await SelecionarTodos(p => p.Id == compraId);
+        }
+
+
+        public virtual async Task<Cota> ObterUmaCotaComSuaCompra(Guid id)
+        {
+            return await _ctx.Cota.AsNoTracking().Include(f => f.Compra)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+
+    }
+}
