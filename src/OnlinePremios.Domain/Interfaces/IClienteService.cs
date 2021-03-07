478ab9c840cfc9272cb585881b3a0cc6a751fcb8
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-07 00:49:11 -0300

    first version of IClienteService ready

---
+++ src/OnlinePremios.Domain/Interfaces/IClienteService.cs
@@ -0,0 +1,15 @@
+﻿using OnlinePremios.Domain.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace OnlinePremios.Domain.Interfaces
+{
+    public interface IClienteService : IDisposable
+    {
+        Task Adicionar(Cliente cliente);
+        Task Atualizar(Cliente cliente);
+        Task Remover(Guid id);
+
+        Task AtualizarCompra(Compra compra);
+    }
+}
