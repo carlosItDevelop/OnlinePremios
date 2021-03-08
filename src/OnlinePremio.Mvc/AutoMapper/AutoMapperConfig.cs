4bd7c21747c4943b9c2c7545aa23c567914d0953
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-07 22:08:56 -0300

    Created AutoMapperConfig and config with ReverseMap Clieente, Compra, Cota, Galeria, Produto and Sorteio

---
+++ src/OnlinePremio.Mvc/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using OnlinePremio.Mvc.ViewModels;
+using OnlinePremios.Domain.Entities;
+
+namespace OnlinePremio.Mvc.AutoMapper
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Cliente, ClienteViewModel>().ReverseMap();
+            CreateMap<Compra, CompraViewModel>().ReverseMap();
+            CreateMap<Cota, CotaViewModel>().ReverseMap();
+
+            CreateMap<Galeria, GaleriaViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+            CreateMap<Sorteio, SorteioViewModel>().ReverseMap();
+        }
+    }
+
+}
