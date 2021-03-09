a33ef70152b61bc5cd6c00d131ed6cdd22e901a8
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-09 04:38:43 -0300

    Add Extension Method SummaryViewComponent

---
+++ src/OnlinePremio.Mvc/Extensions/SummaryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlinePremios.Domain.Interfaces.Notify;
+using System.Threading.Tasks;
+
+namespace OnlinePremio.Mvc.Extensions
+{
+    public class SummaryViewComponent : ViewComponent
+    {
+        private readonly INotificator _notificator;
+
+        public SummaryViewComponent(INotificator notificator)
+        {
+            _notificator = notificator;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var notificacoes = await Task.FromResult(_notificator.GetNotifications());
+            notificacoes.ForEach(c => ViewData.ModelState.AddModelError(string.Empty, c.Mensagem));
+
+            return View();
+        }
+    }
+}
