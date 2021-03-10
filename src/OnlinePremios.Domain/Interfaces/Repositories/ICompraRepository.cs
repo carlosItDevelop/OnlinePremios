66a344a65e6b60d3e3687da3f5d8c3884d887306
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-10 18:29:17 -0300

    Created ICompraRepository

---
+++ src/OnlinePremios.Domain/Interfaces/Repositories/ICompraRepository.cs
@@ -0,0 +1,16 @@
+﻿using OnlinePremios.Domain.Core;
+using OnlinePremios.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OnlinePremios.Domain.Interfaces.Repositories
+{
+    public interface ICompraRepository : IGenericRepository<Compra, Guid>
+    {
+        Task<IEnumerable<Produto>> ObterTodasAsComprasDeUmCliente(Guid clienteId);
+        Task<IEnumerable<Produto>> ObterTodasAsComprasComSeusClientes();
+        Task<Produto> ObterUmaCompraComSeuCliente(Guid id);
+    }
+}
+
