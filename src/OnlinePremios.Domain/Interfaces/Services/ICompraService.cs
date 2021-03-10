4d7b808b01049a65dc82fa05bddd643b6ea0b964
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-10 18:32:29 -0300

    Created ICompraService

---
+++ src/OnlinePremios.Domain/Interfaces/Services/ICompraService.cs
@@ -0,0 +1,13 @@
+﻿using OnlinePremios.Domain.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace OnlinePremios.Domain.Interfaces.Services
+{
+    public interface ICompraService : IDisposable
+    {
+        Task Adicionar(Compra compra);
+        Task Atualizar(Compra compra);
+        Task Remover(Guid id);
+    }
+}
