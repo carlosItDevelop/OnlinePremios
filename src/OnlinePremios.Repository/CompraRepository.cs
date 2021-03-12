f5361d763f33ab4695992e38541e4530dd3eade7
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-12 17:42:58 -0300

    CompraRepository ok. Implementar os métodos

---
+++ src/OnlinePremios.Repository/CompraRepository.cs
@@ -0,0 +1,36 @@
+﻿using OnlinePremios.Data.Orm;
+using OnlinePremios.Domain.Entities;
+using OnlinePremios.Domain.Interfaces.Repositories;
+using OnlinePremios.Repository.Base;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OnlinePremios.Repository
+{
+    public class CompraRepository : RepositoryGeneric<Compra, Guid>, ICompraRepository
+    {
+
+        private readonly OnlinePremiosContext _ctx;
+
+        public CompraRepository(OnlinePremiosContext context) : base(context)
+        {
+            this._ctx = context;
+        }
+
+        public Task<IEnumerable<Compra>> ObterTodasAsComprasComSeusClientes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Compra>> ObterTodasAsComprasDeUmCliente(Guid clienteId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Compra> ObterUmaCompraComSeuCliente(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
