7883a32040eef59762e002bf938b8714b2cb8960
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-12 21:15:17 -0300

    Created SorteioValidation

--- src/OnlinePremios.Domain/Entities/Validations/SorteioValidation.cs
+++ src/OnlinePremios.Domain/Entities/Validations/SorteioValidation.cs
@@ -1,6 +1,24 @@
-﻿namespace OnlinePremios.Domain.Entities.Validations
+﻿using FluentValidation;
+using System;
+
+namespace OnlinePremios.Domain.Entities.Validations
 {
-    public class SorteioValidation
+    public class SorteioValidation : AbstractValidator<Sorteio>
     {
+
+        public SorteioValidation()
+        {
+            RuleFor(f => f.Numero)
+                    .NotNull().WithMessage("O campo {PropertyName} precisa ser fornecido");
+
+            RuleFor(f => f.DataInicial)
+                    .LessThan(DateTime.Now)
+                    .WithMessage("A {PropertyName} não pode ser no tempo passado");
+
+            RuleFor(f => f.DataSorteio)
+                    .NotNull()
+                    .WithMessage("A {PropertyName} não pode ser nula.");
+
+        }
     }
 }
