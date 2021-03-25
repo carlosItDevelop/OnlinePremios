078eb4b3760753bc292908ce790c2e14276368b9
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-24 21:07:13 -0300

    Cota and Sorteio changed for equality with new branch

--- src/OnlinePremios.Domain/Entities/Cota.cs
+++ src/OnlinePremios.Domain/Entities/Cota.cs
@@ -6,28 +6,32 @@ namespace OnlinePremios.Domain.Entities
 {
     public class Cota : EntityBase
     {
-        public Cota(int numero, Guid compraId, decimal valor)
+        public Cota(int numero, decimal valor)
         {
             this.Numero = numero;
-            this.CompraId = compraId;
             this.Valor = valor;
             this.StatusCota = StatusCota.Disponivel;
         }
 
+        /// <summary>
+        /// Ess construto é para adiconar cotas no Sorteio
+        /// </summary>
+        /// <param name="numero">Numeração sequancia das cotas</param>
+        /// <param name="status">Status Inciial da cota</param>
+        public Cota(int numero = 0)
+        {
+            this.Numero = numero;
+
+        }
         // to EF
         public Cota() { }
 
-        public int Numero { get; private set; }
-        public decimal Valor { get; private set; }
-        public StatusCota StatusCota { get; private set; }
-        public Guid CompraId { get; private set; }
+        public int Numero { get; set; }
+        public decimal Valor { get; set; }
+        public StatusCota StatusCota { get; set; }
+        public Guid CompraId { get; set; }
 
         public virtual Compra Compra { get; set; }
-
-
-        // Todo: Cotas para pertencer a Compra, precisa ter outra denominação, ainda que seja a mesma Cota que pegamos do Sorteio.
-        // Todo: Preciso mexer no Map, na Model Compra, nos Services e Repositories;
-        // Todo: Já adicioei uma coleção de Cotas no Sorteio => IEnumerable<Cota> Cota (in Sorteio);
 
     }
 }
--- src/OnlinePremios.Domain/Entities/Sorteio.cs
+++ src/OnlinePremios.Domain/Entities/Sorteio.cs
@@ -1,20 +1,20 @@
 ﻿using OnlinePremios.Domain.Core;
 using OnlinePremios.Domain.Enum;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace OnlinePremios.Domain.Entities
 {
     public class Sorteio : EntityBase
     {
-        protected Sorteio(int numero, DateTime dataInicial, 
-            DateTime dataSorteio, int qtdeCotas)
+        protected Sorteio(int numero, DateTime dataInicial,
+            DateTime dataSorteio, int qtdeCotas, decimal valorCotas)
         {
             this.Numero = numero;
             this.DataInicial = dataInicial;
             this.DataSorteio = dataSorteio;
             this.QtdeCotas = qtdeCotas;
+            this.ValorCotas = valorCotas;
             this.StatusSorteio = StatusSorteio.Ativo;
         }
 
@@ -25,9 +25,27 @@ namespace OnlinePremios.Domain.Entities
         public DateTime DataInicial { get; private set; }
         public DateTime DataSorteio { get; private set; }
         public int QtdeCotas { get; private set; }
+        public decimal ValorCotas { get; private set; }
         public StatusSorteio StatusSorteio { get; set; }
 
-        public virtual IEnumerable<Cota> Cota { get; set; }
+        public virtual IEnumerable<Cota> Cota { get; private set; }
 
+        public void GerarCota(int numero, int QtdeCotas)
+        {
+            List<Cota> cotas = new List<Cota>();
+
+            for (int i = numero; i < QtdeCotas; i++)
+
+            {
+
+                cotas.Add(new Cota(i)
+                {
+                    Numero = i,
+                    StatusCota = StatusCota.Disponivel,
+                    Valor = ValorCotas
+                });
+                //(int numero, Guid compraId, decimal valor)
+            }
+        }
     }
 }
