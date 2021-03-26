845645b1baee2c330ff3ac17b39575d371c02170
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-25 22:35:45 -0300

    Creted Revendedor

---
+++ src/OnlinePremios.Domain/Entities/Revendedor.cs
@@ -0,0 +1,21 @@
+﻿using OnlinePremios.Domain.Core;
+
+namespace OnlinePremios.Domain.Entities
+{
+    public class Revendedor : EntityBase
+    {
+        // To EF
+        public Revendedor() {}
+
+        public Revendedor(string nome, string cpf)
+        {
+            this.Nome = nome;
+            this.cpf = cpf;
+
+        }
+
+        public string Nome { get; private set; }
+        public string cpf { get; private set; }
+        public string Rg { get; private set; }
+    }
+}
