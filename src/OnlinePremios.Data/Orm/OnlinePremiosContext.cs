b02eaa54425b82018ed94f199a57899a55c227f6
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-26 04:20:44 -0300

    Created and comment override SaveChangesAsync in OnlinePremiosContext

--- src/OnlinePremios.Data/Orm/OnlinePremiosContext.cs
+++ src/OnlinePremios.Data/Orm/OnlinePremiosContext.cs
@@ -50,23 +50,23 @@ namespace OnlinePremios.Data.Orm
 
         }
 
-        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
-        {
-            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
-            {
-                if (entry.State == EntityState.Added)
-                {
-                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
-                }
+        //public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        //{
+        //    foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+        //    {
+        //        if (entry.State == EntityState.Added)
+        //        {
+        //            entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+        //        }
 
-                if (entry.State == EntityState.Modified)
-                {
-                    entry.Property("DataCadastro").IsModified = false;
-                }
-            }
+        //        if (entry.State == EntityState.Modified)
+        //        {
+        //            entry.Property("DataCadastro").IsModified = false;
+        //        }
+        //    }
 
-            return base.SaveChangesAsync(cancellationToken);
-        }
+        //    return base.SaveChangesAsync(cancellationToken);
+        //}
 
     }
 }
