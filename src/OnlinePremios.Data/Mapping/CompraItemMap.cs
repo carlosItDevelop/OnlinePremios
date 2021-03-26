865a47d4955bd22ce99edfac9e5f4e1be02539fc
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-26 03:08:19 -0300

    Created CompraItemMap

---
+++ src/OnlinePremios.Data/Mapping/CompraItemMap.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlinePremios.Domain.Entities;
+
+namespace OnlinePremios.Data.Mapping
+{
+    public class CompraItemMap : IEntityTypeConfiguration<CompraItem>
+    {
+        public void Configure(EntityTypeBuilder<CompraItem> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Numero).IsRequired().HasColumnName("Numero");
+            builder.Property(x => x.Valor).IsRequired().HasColumnName("Valor");
+
+            builder.ToTable("CompraItem");
+
+            // 1:N Compras : CompraItem
+            builder.HasOne(x => x.Compra)
+                .WithMany(x => x.CompraItens)
+                .HasForeignKey(x => x.CompraId);
+        }
+    }
+}
