11215f0a639ae08f6fa31c58e5d2d156b3661e48
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-26 04:06:27 -0300

    Created CompraItemValidation

---
+++ src/OnlinePremios.Domain/Entities/Validations/CompraItemValidation.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace OnlinePremios.Domain.Entities.Validations
+{
+    public class CompraItemValidation : AbstractValidator<CompraItem>
+    {
+        public CompraItemValidation()
+        {
+            RuleFor(f => f.Numero)
+                .NotNull().WithMessage("O campo {PropertyName} precisa ser fornecido");
+
+            RuleFor(f => f.CompraId)
+                .NotNull().WithMessage("O campo {PropertyName} precisa ser fornecido");
+
+        }
+    }
+}
