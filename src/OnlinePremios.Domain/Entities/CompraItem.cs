57a09183dfa4950cf878a178b4b91a20dedbc342
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-26 02:49:13 -0300

    CompraItem created and add parameter compraId in ctor

---
+++ src/OnlinePremios.Domain/Entities/CompraItem.cs
@@ -0,0 +1,25 @@
+﻿using OnlinePremios.Domain.Core;
+using System;
+
+namespace OnlinePremios.Domain.Entities
+{
+    public class CompraItem : EntityBase
+    {
+        public CompraItem(int numero, decimal valor, Guid compraId)
+        {
+            this.Numero = numero;
+            this.Valor = valor;
+            this.CompraId = compraId;
+        }
+
+        // to EF
+        public CompraItem() { }
+
+        public int Numero { get; set; }
+        public decimal Valor { get; set; }
+        public Guid CompraId { get; set; }
+
+        public virtual Compra Compra { get; set; }
+
+    }
+}
