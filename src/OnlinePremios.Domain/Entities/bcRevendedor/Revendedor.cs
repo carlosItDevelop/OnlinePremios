70d35eeae22fad4abe4c1d68a54788396c99f7c7
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-26 21:52:42 -0300

    cpf to Cpf in Revendedor

--- src/OnlinePremios.Domain/Entities/bcRevendedor/Revendedor.cs
+++ src/OnlinePremios.Domain/Entities/bcRevendedor/Revendedor.cs
@@ -11,7 +11,7 @@ namespace OnlinePremios.Domain.Entities.bcRevendedor
         public Revendedor(string nome, string cpf, string email, string rg, TipoPessoa tipoPessoa, StatusRevendedor status)
         {
             this.Nome = nome;
-            this.cpf = cpf;
+            this.Cpf = cpf;
             this.Email = email;
             this.Rg = rg;
             this.TipoPessoa = tipoPessoa;
@@ -19,7 +19,7 @@ namespace OnlinePremios.Domain.Entities.bcRevendedor
         }
 
         public string Nome { get; private set; }
-        public string cpf { get; private set; }
+        public string Cpf { get; private set; }
         public string Rg { get; private set; }
         public string Email { get; private set; }
         public TipoPessoa TipoPessoa { get; private set; }
