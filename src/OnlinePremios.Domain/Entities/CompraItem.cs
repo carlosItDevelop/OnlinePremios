6f92e197ec90e9ccf95e8d92de21453f53696c39
Antonio Nelson <antoniooracio@gmail.com>
2021-03-28 11:40:04 -0300

    Atualização CompraItem

--- src/OnlinePremios.Domain/Entities/CompraItem.cs
+++ src/OnlinePremios.Domain/Entities/CompraItem.cs
@@ -1,25 +1,21 @@
 ﻿using OnlinePremios.Domain.Core;
 using OnlinePremios.Domain.Enum;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace OnlinePremios.Domain.Entities
 {
     public class CompraItem : EntityBase
     {
-        public CompraItem(int numero, decimal valor)
-        {
-            this.Numero = numero;
-            this.Valor = valor;
-        }
-
-        // to EF
-        public CompraItem() { }
-
+        [Key]
+        public string ItemId { get; set; }
         public int Numero { get; set; }
-        public decimal Valor { get; set; }
         public Guid CompraId { get; set; }
-
+        public System.DateTime DateCriação { get; set; }
+        public Guid CotaId { get; set; }
         public virtual Compra Compra { get; set; }
+        public virtual Cota Cota { get; set; }
+
 
     }
 }
