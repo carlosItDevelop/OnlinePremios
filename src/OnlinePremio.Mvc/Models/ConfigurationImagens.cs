a801dee39b3cae055d17cb049c49f1b8edfd5dac
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-03-31 20:42:56 -0300

    Created ConfigurationImagens in Models/Mvc and configured in startup

---
+++ src/OnlinePremio.Mvc/Models/ConfigurationImagens.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlinePremio.Mvc.Models
+{
+    public class ConfigurationImagens
+    {
+        public string NomePastaImagensProdutos { get; set; }
+    }
+}
